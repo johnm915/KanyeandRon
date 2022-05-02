597300d0ae7ae9fa241a2941fc09faed88b6be32
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-05-02 11:39:08 -0500

    DONE

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.kanyeQuote();
+
+            QuoteGenerator.RonQuote();
+            
+       }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public static void kanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanysURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanysURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"Kanye:  '{kanyeQuote}'");
+            Console.WriteLine("----------------------------------------------------------");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var RonResponse = client.GetStringAsync(ronURL).Result;
+
+           
+
+            var RonQuote = JArray.Parse(RonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"Ron:  '{RonQuote}'");
+            Console.WriteLine("----------------------------------------------------------");
+        }
+
+    }
+}
